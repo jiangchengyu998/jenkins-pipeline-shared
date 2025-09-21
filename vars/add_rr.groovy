33ba4ff657b7fb25a98d325cb43664b6c30c937d
hanke <jchengyu0829@163.com>
2025-09-21 14:45:23 +0800

    ddd

--- vars/add_rr.groovy
+++ vars/add_rr.groovy
@@ -7,19 +7,26 @@ def call(Map config = [:]) {
             string(name: 'exe_node', defaultValue: config.exe_node ?: 'w-ubuntu', description: 'node')
         }
 
-
         stages {
-            stage('list RR') {
+            stage('check RR existence') {
                 steps {
                     script {
-                        sh "aliyun alidns DescribeDomainRecords --region public --DomainName 'ydphoto.com'"
-                    }
-                }
-            }
-            stage('add RR') {
-                steps {
-                    script {
-                        sh "aliyun alidns DescribeDomainRecords --region public --DomainName 'ydphoto.com' --RRKeyWord ${params.RR}"
+                        def checkResult = sh(
+                                script: "aliyun alidns DescribeDomainRecords --region public --DomainName 'ydphoto.com' --RRKeyWord ${params.RR}",
+                                returnStdout: true
+                        )
+
+                        // 检查返回结果中是否包含指定的RR记录
+                        if (checkResult.contains('"TotalCount": 0')) {
+                            echo "RR record ${params.RR} does not exist. Adding it now..."
+                            // 如果不存在，则添加RR记录
+                            sh "aliyun alidns AddDomainRecord --region public --DomainName 'ydphoto.com' --RR ${params.RR} --Type A --Value '8.138.212.208'"
+                        } else {
+                            echo "RR record ${params.RR} already exists. Exiting..."
+                            // 如果存在，则结束流程
+                            currentBuild.result = 'SUCCESS'
+                            return
+                        }
                     }
                 }
             }
@@ -27,10 +34,10 @@ def call(Map config = [:]) {
 
         post {
             success {
-                echo "Deployment completed successfully on port ${params.API_PORT}"
+                echo "Operation completed successfully"
             }
             failure {
-                echo "Deployment failed"
+                echo "Operation failed"
             }
         }
     }
